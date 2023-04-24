d9b763448bc39cbf1ba864fc7b3638a94e318413
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-04-25 00:44:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const server = net.createServer((socket) => {
   });
 });
 
-server.listen(0, '3.75.158.163', () => {
+server.listen(0, 'https://collapsegameserver.onrender.com', () => {
   console.log('sa as');
 });
