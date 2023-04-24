bb4f909fd5ac35ee476cdea692360821751e22a2
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-04-25 00:34:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('sa');
+const net = require('net');
+
+const server = net.createServer((socket) => {
+  console.log('Client connected');
+
+  socket.on('data', (data) => {
+    console.log(`Received message: ${data}`);
+  });
+
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, '3.75.158.163', () => {
+  console.log('sa as');
+});
